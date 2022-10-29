92c48f3a495a8d188cb4f6d7bdf9518853eb5a7d
Mark Barbaric <mark.barbaric@flextrade.com>
2022-10-29 16:47:29 +0100

    Initial Commit

---
+++ Chapter4/circle_polygon.py
@@ -0,0 +1,34 @@
+# %%
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def show_with_matplotlib(img, title):
+    img_RGB = img[:, :, ::-1]
+
+    plt.imshow(img_RGB)
+    plt.title(title)
+    plt.show()
+
+colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (0, 255, 255),
+          'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0),
+          'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
+          'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
+
+image = np.zeros((640, 640, 3), dtype= "uint8")
+
+image.fill(255)
+
+pts = np.array(
+    [(600, 320), (563, 460), (460, 562), (320, 600), (180, 563), (78, 460), (40, 320), (77, 180), (179, 78), (319, 40),
+     (459, 77), (562, 179)])
+
+plts = pts.reshape((-1, 1, 2))
+
+cv2.polylines(image, [pts], True, colors['green'], 5)
+
+print("hello")
+show_with_matplotlib(image, 'polyfon with the shapre of a circle using 12 points')
+
+
+# %%
